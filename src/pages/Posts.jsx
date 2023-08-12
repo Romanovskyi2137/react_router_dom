236cb6057f26d3882f2c2e4970bb2ea5bbddc7b7
Romanovskyi2137 <113349151+Romanovskyi2137@users.noreply.github.com>
2023-08-12 16:01:47 +0300

    update posts.jsx

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -2,14 +2,26 @@ import React, { useState, useEffect } from "react";
 import {Link, useSearchParams, useNavigate} from "react-router-dom";
 import PostFilter from "../components/PostFilter";
 
+
+
 export function Posts (props) {
     const [searchParams, setSearchParams] = useSearchParams();
     const [posts, setPosts] = useState([]);
+    const sessionData = JSON.parse(sessionStorage.getItem("posts"));
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(data => setPosts(data))
-        }, []);    
+        if (!sessionData) {
+            fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(data => {
+                setPosts(data);
+                sessionStorage.setItem("posts", JSON.stringify(data));
+            })
+            console.log("get data with fetch")
+    } else {
+        console.log("get data from session storage")
+        setPosts(sessionData)
+    }
+    }, []);
     const styles = {
         display: "flex",
         justifyContent: "center",
